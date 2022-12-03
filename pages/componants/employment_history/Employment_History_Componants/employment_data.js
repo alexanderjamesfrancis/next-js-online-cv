339396b5f21096e4e8707d3bb50da33619b2667b
Alex Francis <alexanderjamesfrancis@hotmail.co.uk>
2022-12-03 10:17:43 +0000

    Finish employment_data

--- pages/componants/employment_history/Employment_History_Componants/employment_data.js
+++ pages/componants/employment_history/Employment_History_Componants/employment_data.js
@@ -14,16 +14,117 @@ export default function employment_data() {
     //   }],
     // },
     {
-      companyName: "Fortis Student Living – Nottingham",
-      dateWorked: "January 2017 – May 2017",
-      roleWorked: "Accommodation Manager",
+      companyName: "Big Tyres LTD - Nottingham ",
+      dateWorked: "November 2022 – Present",
+      roleWorked: "Data Administration Services (Part Time)",
+      descriptionHeader:
+        "Due to my positive history with Big Tyres LTD, I was asked if I would like to return in a temporary measure to provide data services for the e-commerce back-end. This role is allowing me some practical interaction with data whilst allowing me to continue my Web Development training.",
+      description: [
+        {
+          header: "New Product Entry and Updating",
+          content: [
+            "Taking new product files and reformatting data to adhere to pre-defined schema and updating existing product lines with new SKUs.",
+            "Researching relevant information for new products. E.G EU tyre label for eligible products or finding assets and information in order to properly market new products.",
+            "Both actions require heavy use of excel and PHP import tools to a Magento back-end.",
+            "Bug fixing, if there are any issues with the data import. ",
+          ],
+        },
+        {
+          header: "Inter-Departmental Relations",
+          content: [
+            "Liaising with the Sales and Customer Services manager on adding relevant products and marketing direction of new brands and products.",
+            "Requesting training on best practice for new items in order for data to be consistent and scalable in future from Web Development Lead.",
+            "Working with suppliers when required to aquire product specific information.",
+          ],
+        },
+      ],
+    },
+    {
+      companyName: "Full Stack Web Development Sabbatical – Nottingham",
+      dateWorked: "December 2021 – October 2022",
+      roleWorked: "In Training",
+      descriptionHeader:
+        "Reaching the conclusion of my previous role, I have taken time for personal development and retraining. This time has been spent learning Front-End Web Development and programming. The skills I have learnt are as follows, with further knowledge growing daily: ",
+      description: [
+        {
+          header: "Front End Website Skills",
+          content: [
+            "HTML",
+            "CSS (with Tailwind and Bootstrap basics)",
+            "Javascript",
+            "React",
+            "EJS",
+          ],
+        },
+        {
+          header: "Back-End Building",
+          content: [
+            "Node",
+            "Express",
+            "Flask",
+            "Git and Version control",
+            "Familiarity with databases such as MongoDB using ORMs (such as Mongoose)",
+            "API usage and management",
+          ],
+        },
+        {
+          header: "Python",
+          content: [
+            "General logical operators and rudimentary programs",
+            "Tkinter",
+            "Data processing using modules such as Beautiful Soup and Pandas",
+            "Selenium",
+            "OOP",
+          ],
+        },
+      ],
+    },
+    {
+      companyName: "Big Tyres LTD – Nottingham",
+      dateWorked: "September 2020 – September 2021",
+      roleWorked: "Sales and Customer Service Manager",
+      descriptionHeader:
+        "Due to my success with my previous role, I was promoted within the business. As an extension of my previous role, additional responsibilities included:",
+      description: [
+        {
+          header: "Staff Management",
+          content: [
+            "Managing a small team of Sales and Customer Service staff to deliver excellent service and drive sales for the business.General Aspects of logical operators",
+            "Offering appropriate training to staff on new products and processes that will affect the business.",
+            "Handling staff issues such as holiday and team meetings.",
+          ],
+        },
+        {
+          header: "Sales",
+          content: [
+            "Managing the Sales across the team to meet quarterly targets agreed with upper management.",
+            "Identifying sales opportunities to excel profit.",
+            "Managing high pressure/high profit orders personally or supporting the Sales and Customer Service team from sale inception to conclusion.",
+          ],
+        },
+        {
+          header: "Business to Business Relationships",
+          content: [
+            "Engaging with Supplier representatives to maintain database, pricelists and negotiating best pricing for orders of mutual benefit.",
+            "Working with established networks of contacts to source and supply specialist items as required.",
+            "Understanding of supply chains from manufacturing to end product - pressure points, expectations and repercussions that can affect the business.",
+            "Working on establishing accounts and relationships with new suppliers.",
+            "Know where the limits of the business relationship lie and ensure they are respected.",
+          ],
+        },
+      ],
+    },
+    {
+      companyName: "Big Tyres LTD – Nottingham",
+      dateWorked: "June 2017 – September 2020",
+      roleWorked: "Sales and Customer Service Representative",
       descriptionHeader: "",
       description: [
         {
           header: "Service Management",
           content: [
             "Maintaining B2B relationships with our suppliers to manage orders and promote coordination to deliver goods to the end user.",
-            "Maintaining B2B relationships with our suppliers to manage orders and promote coordination to deliver goods to the end user.",
+            "Liaising with service providers to keep customer who require work conducted on site up to date.",
             "Building working relationships with longer term customers to maintain rapport and ongoing Sales.",
           ],
         },
@@ -32,7 +133,7 @@ export default function employment_data() {
           content: [
             "Taking customer inquiries via a variety of means such as phone, email and Livechat.",
             "Interpreting the information provided to direct the customer to the appropriate product and identify opportunities for upselling as required.",
-            "Handling both cash sales for goods and taking payment via multiple platforms.",
+            "Handling both cash sales for goods and taking payment via multiple platforms",
             "Making sure all processes are followed when dealing with customers outside of the UK and ensuring VAT compliance (Pre-Brexit).",
           ],
         },
@@ -47,10 +148,44 @@ export default function employment_data() {
       ],
     },
     {
+      companyName: "Fortis Student Living – Nottingham",
+      dateWorked: "January 2017 – May 2017",
+      roleWorked: "Accommodation Manager",
+      descriptionHeader: "",
+      description: [
+        {
+          header: "Facilities Management",
+          content: [
+            "Management of building faults through dedicated reactive system.",
+            "Responsible for day-to-day operation of a 102 room student accommodation block with duties conducted on an ad-hoc basis on three further buildings.",
+            "Liaising with contractors on work conducted.",
+          ],
+        },
+        {
+          header: "Sales",
+          content: [
+            "Handling of inquiry from initial interest to finalizing deposit on bookings.",
+            "Answering inquiries via several methods including email, telephone, face-to-face and text where required.",
+            "Providing detailed information on all Nottingham Properties, delegating enquiries where necessary to colleagues.",
+          ],
+        },
+        ,
+        {
+          header: "Customer Service",
+          content: [
+            "Answering any on-site questions for residents and managing any complaints voiced.",
+            "Creating bookings and amending according to customer preference.",
+            "Providing concierge service by providing information on local area, receiving parcels for residents and managing social media presence.",
+          ],
+        },
+      ],
+    },
+    {
       companyName: "Premier Inn, Warwick – Thyme",
       dateWorked: "December 2014 – December 2016",
       roleWorked: "Duty Manager",
-      descriptionHeader: "Based on my performance and customer service skills from being a Team Member, I was promoted to Duty Manager within Premier Inn:",
+      descriptionHeader:
+        "Based on my performance and customer service skills from being a Team Member, I was promoted to Duty Manager within Premier Inn:",
       description: [
         {
           header: "Safety and Security",
