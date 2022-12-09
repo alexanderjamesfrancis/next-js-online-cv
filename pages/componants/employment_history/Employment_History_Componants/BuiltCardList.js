9269b745b075fcca698bcad53a6c47fede5180dc
Alex Francis <alexanderjamesfrancis@hotmail.co.uk>
2022-12-09 11:59:31 +0000

    Employment_History_Componants continues

--- pages/componants/employment_history/Employment_History_Componants/BuiltCardList.js
+++ pages/componants/employment_history/Employment_History_Componants/BuiltCardList.js
@@ -4,19 +4,14 @@ import CardBuilder from "./CardBuilder"
 
 export default function BuiltCardList(role){
 
+    //console.log(role.role);
+    const newRole = role.role
 
-    console.log(role.role);
+    return (<CardBuilder props={newRole} />)
+}
+
+
     
-    const thing = role.role
     
 
-    return(
-        <div></div>
-        // {role.map((item,index)=>{
-        //     return(<CardBuilder role = {item} key={index} />) 
-        // })}
-    )
-
-
-
-    }
+ 
--- pages/componants/employment_history/Employment_History_Componants/CardBuilder.js
+++ pages/componants/employment_history/Employment_History_Componants/CardBuilder.js
@@ -1,19 +1,21 @@
 import RoleList from "./RoleList";
 
-export default function CardBuilder(role) {
+export default function CardBuilder(props) {
     //console.log(role);
 
-  const [header, content] = role.role
-
+  // const [header, content] = role.role
+  const head = props.props[0].header
+  console.log(props.props[0].content);
+  const content = props.props[0].content
     //console.log(header);
   //console.log(content.content);
   return (
     
     <div className="space-y-2">
-      <h4 className="text-lg underline underline-offset-1">{header.header}</h4>
+      <h4 className="text-lg underline underline-offset-1">{head}</h4>
       <div>
 
-        {content.content.map((item, index) => {
+        {content.map((item, index) => {
             return(<RoleList roleItem={item} key={index} />)
         })}
       </div>
