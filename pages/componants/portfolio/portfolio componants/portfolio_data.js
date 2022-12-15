429081be66ae7946a183c1760c3ba4a4e771b6b9
Alex Francis <alexanderjamesfrancis@hotmail.co.uk>
2022-12-15 16:02:48 +0000

    Completed portfolio_data

--- pages/componants/portfolio/portfolio componants/portfolio_data.js
+++ pages/componants/portfolio/portfolio componants/portfolio_data.js
@@ -2,10 +2,63 @@ export default function portfolioData(){
     return(
 
         //Data Model:
+        // {
+        //     link: "link",
+        //     desktopImage: "desktop image",
+        //     mobileImage: "mobile Image",
+        //     cardName: "Card Name",
+        //     description: "Something about this card"
+        // }
         
 
         [{
-            data: "key"
+            link: "https://alexanderjamesfrancis.github.io/jethroTime/",
+            desktopImage: "images/desktop/jethrotime-desktop.jpeg",
+            mobileImage: "images/mobile/Late-person.jpg",
+            cardName: "JethroTime",
+            description: "A silly time predictor for my friend, Jethro"
+        },
+         {
+            link: "https://github.com/alexanderjamesfrancis/ondemandpeachbot",
+            desktopImage: "images/desktop/bed-peach.jpg",
+            mobileImage: "images/mobile/peach.jpg",
+            cardName: "PeachBot",
+            description: "A Discord bot made using Python to summon pictures of Peach the Cat"
+        },
+        {
+            link: "https://alexanderjamesfrancis.github.io/calling-card/",
+            desktopImage: "images/desktop/calling-card-desktop.jpg",
+            mobileImage: "images/mobile/calling-card-mobile.jpg",
+            cardName: "Calling Card",
+            description: "A simple digital business card, available from anywhere!"
+        },
+        {
+            link: "https://github.com/alexanderjamesfrancis/dungeon_text_adventure",
+            desktopImage: "images/desktop/desltop-fantasy-landscape.jpg",
+            mobileImage: "images/mobile/fantasy-landscape.jpg",
+            cardName: "Fantasy Text Adventure",
+            description: "A work in progress, text-based RPG"
+        },
+        {
+            link: "https://github.com/alexanderjamesfrancis/cookie_clicker_autoclicker",
+            desktopImage: "images/desktop/cookie-desktop.jpg",
+            mobileImage: "images/mobile/cookie-mobile.jpg",
+            cardName: "Cookie Clicker Clicker",
+            description: "An automated bot made using Selenium to play Cookie Clicker"
+        },
+        {
+            link: "/",
+            desktopImage: "images/desktop/injury-deadpool-desktop.png",
+            mobileImage: "images/mobile/injury-deadpool-mobile.png",
+            cardName: "The Injury Deadpool",
+            description: "A web application to keep track of and guess the next climbing injury earning points in the process (Coming Soon)"
+        },
+        {
+            link: "https://weather-app-ten-blush.vercel.app/",
+            desktopImage: "images/desktop/weather-icon.png",
+            mobileImage: "images/mobile/weather-icon.png",
+            cardName: "Weather App",
+            description: "Made using NextJs and React"
         }]
     )
 }
