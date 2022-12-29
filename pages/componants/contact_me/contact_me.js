2a5ede4e5e84ff7a4e559dfb081325f8cd7b70eb
Alex Francis <alexanderjamesfrancis@hotmail.co.uk>
2022-12-29 12:52:21 +0000

    Made a form

--- pages/componants/contact_me/contact_me.js
+++ pages/componants/contact_me/contact_me.js
@@ -1,5 +1,6 @@
 import Header from "../page_defaults/Header";
 import Footer from "../page_defaults/Footer";
+import Image from "next/image";
 
 export default function Contact_Me() {
   return (
@@ -17,6 +18,36 @@ export default function Contact_Me() {
           <p>Home Address: Hucknall, Nottingham.</p>
           <p>Mobile: 07985 921903</p>
           <p>E-mail: alexanderjamesfrancis@hotmail.co.uk</p>
+        </div>
+        <div>
+          <div>
+            <h4>Send me a message!</h4>
+          </div>
+          <div className="">
+            <form className="flex flex-col items-center">
+              <div className="form-item">
+                <label>Your Name</label>
+                <input className="rounded-md border-2" type="text" required />
+              </div>
+              <div className="form-item">
+                <label>Email</label>
+                <input type="text" required/>
+              </div>
+              <div className="form-item">
+                <label>Contact Number</label>
+                <input type="text" required/>
+              </div>
+              <div className="form-item">
+                <label>Your Message</label>
+                <textarea rows="4" required />
+              </div>
+              <div className="flex flex-row space-x-2 bg-white hover:bg-black transition ease-in hover:text-white border-2 m-4 px-6 py-2 border-black rounded-full">
+                <button className="" formMethod="POST" type="submit">Submit</button>
+                <Image className="hover:fill-white" width="20" height="20" src="/icons/envelope-solid.svg"/>
+              </div>
+              
+            </form>
+          </div>
         </div>
       </div>
       <Footer />
