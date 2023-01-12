e2645519a4e3bab024f55a2ad28389f12df75a99
Alex Francis <alexanderjamesfrancis@hotmail.co.uk>
2023-01-12 15:12:13 +0000

    Check API status

--- pages/api/contact_me_email.js
+++ pages/api/contact_me_email.js
@@ -4,21 +4,24 @@ sendGrid.setApiKey(process.env.ANOTHER_API_KEY);
 
 async function sendEmail(req,res){
     console.log("You got this far");
+    //console.log("1." + res.statusCode);
     try{
         console.log("Its trying to do the thing");
         await sendGrid.send({
             to: "alexanderjamesfrancis@hotmail.co.uk", // Change to your recipient
             from: "alexthebigfishmk2@gmail.com", // Change to your verified sender
             subject: "Your have received a message from your website",
-            text: `${req.body.content}`, 
+            text: "Cheese", 
             html: "<strong>and easy to do anywhere, even with Node.js</strong>",
         })
         console.log("It did the thing");
     }
     catch (error) {
+        console.log("2." + res.statusCode);
         return res.status(error.statusCode || 500).json({ error: error.message });
     }
     //console.log(res.status);
+    console.log("3." + res.statusCode);
     return res.status(200).json({ error: "" });
 }
 
